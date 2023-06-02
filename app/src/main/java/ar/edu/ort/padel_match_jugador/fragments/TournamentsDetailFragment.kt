06f34ff2ff4e68ea3ac06365a59760ba9fc23ede
Lucas Krigun <lucaskrigun@hotmail.com>
2023-06-01 23:39:13 -0300

    Avance detail  tournament fragment y layout

--- app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/TournamentsDetailFragment.kt
+++ app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/TournamentsDetailFragment.kt
@@ -17,6 +17,7 @@ import android.widget.TextView
 import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import ar.edu.ort.padel_match_jugador.entities.Club
 import com.google.firebase.firestore.ktx.firestore
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.google.firebase.auth.FirebaseAuth
@@ -31,16 +32,15 @@ class TournamentsDetailFragment : Fragment() {
 
 
     private lateinit var v: View
-    private lateinit var titulo: TextView
     private lateinit var detailNombre: TextView
-    private lateinit var detailFecha: TextView
+    private lateinit var detailTitulo: TextView
+    private lateinit var detailFechaInicio: TextView
     private lateinit var detailCategorias: TextView
     private lateinit var detailHorario: TextView
+    private lateinit var detailDireccion: TextView
+    private lateinit var detailLocalidad: TextView
     private lateinit var detailCupos: TextView
-    private lateinit var detailMateriales: TextView
-    private lateinit var detailCostoInscripcion: TextView
-    private lateinit var detailClub: TextView
-    private lateinit var detailPremio: TextView
+    private lateinit var detailCancha: TextView
     private lateinit var detailImagen: TextView
     private lateinit var imageDisplay : ImageView
     private lateinit var viewModel: TournamentsDetailViewModel
@@ -55,31 +55,25 @@ class TournamentsDetailFragment : Fragment() {
     ): View? {
         v = inflater.inflate(R.layout.fragment_tournaments_detail, container, false)
 
-        titulo = v.findViewById(R.id.detail_tituloNombre)
+        detailNombre = v.findViewById(R.id.textNombre)
 
-        detailNombre = v.findViewById(R.id.detail_name)
+        detailTitulo = v.findViewById(R.id.textTitulo)
 
-        detailFecha = v.findViewById(R.id.detail_date)
+        detailFechaInicio = v.findViewById(R.id.fechaInicio)
 
-        detailClub = v.findViewById(R.id.detail_club)
+        detailCategorias = v.findViewById(R.id.categoria)
 
-        detailCategorias = v.findViewById(R.id.detail_categorias)
+        detailHorario = v.findViewById(R.id.horario)
 
-        detailHorario = v.findViewById(R.id.detail_hour)
+        detailDireccion = v.findViewById(R.id.direccion)
 
-        detailCupos = v.findViewById(R.id.detail_cupos)
+        detailLocalidad = v.findViewById(R.id.localidad)
 
-        detailMateriales = v.findViewById(R.id.detail_materiales)
+        detailCupos = v.findViewById(R.id.cupos)
 
-        detailCostoInscripcion = v.findViewById(R.id.detail_costoInscripcion)
+        detailCancha = v.findViewById(R.id.cancha)
 
-        detailPremio = v.findViewById(R.id.detail_premio)
 
-        detailImagen = v.findViewById(R.id.ImagenEditTextDetail)
-
-        imageDisplay = v.findViewById(R.id.imagenDetailTorneo)
-
-        blockFields()
 
         return v
     }
@@ -117,53 +111,20 @@ class TournamentsDetailFragment : Fragment() {
 
     }
 
-    private fun setValues(tournamentSelected: Tournament) {
+    private fun setValues(tournamentSelected: Tournament, clubSelected: Club) {
         Log.w("Torneo selecionado", tournamentSelected.toString())
-        titulo.text = tournamentSelected.titulo
-        detailNombre.setText(tournamentSelected.titulo)
-        detailClub.setText(tournamentSelected.club)
-        detailFecha.setText(tournamentSelected.fecha)
+
+        detailNombre.setText(tournamentSelected.club)
+        detailTitulo.setText(tournamentSelected.titulo)
+        detailFechaInicio.setText(tournamentSelected.fecha)
         detailCategorias.setText(tournamentSelected.categoría)
         detailHorario.setText(tournamentSelected.hora)
+        detailDireccion.setText(clubSelected.domicilio)
+        detailLocalidad.setText(clubSelected.localidad)
         detailCupos.setText(tournamentSelected.cupos.toString())
-        detailCostoInscripcion.setText(tournamentSelected.costoInscripción.toString())
-        detailPremio.setText(tournamentSelected.premios)
-        detailMateriales.setText(tournamentSelected.materialCancha)
+        detailCancha.setText(tournamentSelected.materialCancha)
+     //   Glide.with(this).load(tournamentSelected.imagenTorneo).into(imageDisplay)
 
-    }
-
-
-    private fun blockFields() {
-        titulo.isEnabled = false
-        detailNombre.isEnabled = false
-        detailFecha.isEnabled = false
-        detailCategorias.isEnabled = false
-        detailHorario.isEnabled = false
-        detailCupos.isEnabled = false
-    }
-
-    private fun createTournament(): Tournament {
-
-        val nombre = detailNombre.text.toString();
-        val club = detailClub.text.toString();
-        val date = detailFecha.text.toString();
-        val hour = detailHorario.text.toString();
-        val category = detailCategorias.text.toString();
-        val material = detailMateriales.text.toString();
-        val cupo = detailCupos.text.toString().toInt()
-        val cost = detailCupos.text.toString().toInt();
-        val premio = detailPremio.text.toString();
-        val udi = auth.currentUser!!.uid
-        var idClub = ""
-
-        lifecycleScope.launch {
-            idClub = viewModel.getIdClubByName(club)
-        }
-
-        val retorno = Tournament( nombre, club, date, hour, category, material, cupo, cost, premio,
-            "loading...", udi, idClub )
-
-        return retorno
     }
 
 }
