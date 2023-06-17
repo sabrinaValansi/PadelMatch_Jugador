3b1fe5e71ac762e4a2ebf1c63031d5c6ae8646e2
Lucas Krigun <lucaskrigun@hotmail.com>
2023-06-17 01:51:12 -0300

    filtros

--- app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/Filters.kt
+++ app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/Filters.kt
@@ -2,11 +2,22 @@ package ar.edu.ort.padel_match_jugador.fragments
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.EditText
+import androidx.lifecycle.lifecycleScope
 import ar.edu.ort.padel_match_jugador.R
+import ar.edu.ort.padel_match_jugador.databinding.FragmentFiltersBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.timepicker.MaterialTimePicker
+import kotlinx.coroutines.launch
+import java.util.*
+
 
 class Filters : Fragment() {
 
@@ -15,18 +26,87 @@ class Filters : Fragment() {
     }
 
     private lateinit var viewModel: FiltersViewModel
+    private lateinit var binding: FragmentFiltersBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_filters, container, false)
+        binding = FragmentFiltersBinding.inflate(inflater, container, false)
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FiltersViewModel::class.java)
-        // TODO: Use the ViewModel
+        val datePicker = viewModel.createDatePicker()
+        datePickerHandler(datePicker, binding.editTextFiltroFechaDesde)
+        datePickerHandler(datePicker, binding.editTextFiltroFechaHasta)
+
+        val timePicker = viewModel.createTimePicker()
+        timePickerHandler(timePicker, binding.editTextAddTournamentHorario)
+
+        lifecycleScope.launch {
+            var clubs = viewModel.getPartidosList()
+            val data = clubs.map { t -> t.data["nombre"] } as List<String>
+            var list = data.toTypedArray();
+
+            (binding.filListaPartidos as? MaterialAutoCompleteTextView)?.setSimpleItems(list)
+
+            binding.filListaPartidos.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, view, position, id ->
+                    binding.filListaLocalidades.setText("")
+                    val selecPartido = binding.filListaPartidos.text.toString()
+                    val selecClubs = clubs.filter { t -> t.data["nombre"] == selecPartido }
+
+                    selecClubs.forEach{ t ->
+                        val lista = t.data["localidades"] as List<String>
+                        val localidades = lista.toTypedArray()
+                        (binding.filListaLocalidades as? MaterialAutoCompleteTextView)?.setSimpleItems(localidades)
+                    }
+                }
+
+            var data_cat = viewModel.getCategoriasList()
+            ( binding.editTextAddTournamentCategorias as? MaterialAutoCompleteTextView)?.setSimpleItems(data_cat)
+        }
+
+        binding.btnFiltrar.setOnClickListener {
+            lifecycleScope.launch {
+                viewModel.getFilteredMatches(
+                    binding.editTextFiltroFechaDesde.text.toString(),
+                    binding.editTextFiltroFechaHasta.text.toString(),
+                    binding.editTextAddTournamentHorario.toString(),
+                    binding.filListaPartidos.text.toString(),
+                    binding.filListaLocalidades.text.toString(),
+                    binding.editTextAddTournamentCategorias.text.toString()
+                )
+            }
+        }
+
+
+    }
+
+    private fun datePickerHandler(datePicker: MaterialDatePicker<Long>, item: EditText) {
+        item.setOnClickListener{
+            datePicker.show(requireActivity().supportFragmentManager, "tag" )
+            datePicker.addOnPositiveButtonClickListener { selection ->
+                val dateString = DateFormat.format("dd/MM/yyyy", Date(selection)).toString()
+                item.setText(dateString)
+            }
+        }
+    }
+
+    private fun timePickerHandler(timePicker: MaterialTimePicker, item: EditText){
+        item.setOnClickListener {
+            timePicker.show(requireActivity().supportFragmentManager, "tag")
+            timePicker.addOnPositiveButtonClickListener {
+                val hour = timePicker.hour
+                val minute = timePicker.minute
+                binding.editTextAddTournamentHorario.setText(String.format("%02d:%02d", hour, minute))
+            }
+        }
     }
 
 }
--- app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/FiltersViewModel.kt
+++ app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/FiltersViewModel.kt
@@ -1,7 +1,95 @@
 package ar.edu.ort.padel_match_jugador.fragments
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import ar.edu.ort.padel_match_jugador.entities.Tournament
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+import java.util.*
 
 class FiltersViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val db = Firebase.firestore
+
+    fun createDatePicker(): MaterialDatePicker<Long> {
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
+        return MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Seleccione una fecha")
+            .setSelection(today)
+            .setCalendarConstraints(
+                CalendarConstraints.Builder()
+                    .setValidator(DateValidatorPointForward.from(today))
+                    .build()
+            )
+            .build();
+    }
+
+    fun createTimePicker() : MaterialTimePicker {
+        return MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setTitleText("Seleccione una hora")
+            .build()
+    }
+
+    suspend fun getPartidosList(): QuerySnapshot {
+        val query =  db.collection("partidos")
+        val clubs = query.get().await();
+
+        return clubs;
+    }
+
+    suspend fun getCategoriasList(): Array<String> {
+        val query =  db.collection("categorias")
+        val categorias = query.get().await();
+        val data = categorias.map { t -> t.data["nombreCategoria"] }[0] as List<String>
+        var list = data.toTypedArray()
+        return list;
+    }
+
+    // En FiltersViewModel
+    suspend fun getFilteredMatches(
+        fechaDesde: String?,
+        fechaHasta: String?,
+        horario: String?,
+        partido: String?,
+        localidad: String?,
+        categoria: String?
+    ) {
+        val query = db.collection("tournaments")
+
+        fechaDesde?.let {
+            query.whereGreaterThanOrEqualTo("fecha", fechaDesde)
+        }
+        fechaHasta?.let {
+            query.whereLessThanOrEqualTo("fecha", fechaHasta)
+        }
+        horario?.let {
+            query.whereEqualTo("hora", horario)
+        }
+       /*  partido?.let {
+            query.whereEqualTo("partido", partido)
+        }
+        localidad?.let {
+            query.whereEqualTo("localidad", localidad)
+        } */
+        categoria?.let {
+            query.whereEqualTo("categoria", categoria)
+        }
+
+        val result = query.get().await()
+        // val matches = result.toObjects(Tournament::class.java)
+
+        Log.w("TORNEOS FILTRADOS", result.documents.toString())
+
+       //  return matches
+    }
+
+
 }
