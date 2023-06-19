81835d4c0e8992b053adddd3360401e6e583d709
angryPotatoFace <88639361+angryPotatoFace@users.noreply.github.com>
2023-06-19 17:25:31 -0300

    Error filtro

--- app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/TournamentsFragment.kt
+++ app/src/main/java/ar/edu/ort/padel_match_jugador/fragments/TournamentsFragment.kt
@@ -160,13 +160,12 @@ class TournamentsFragment : Fragment() {
             adapter = TournamentAdapter(requireContext()) { pos ->
                 onItemClick(pos)
             }
-
-
-           /* if (  TournamentsFragmentArgs.fromBundle(requireArguments()).tournamentList != null) {
+            
+           if (  TournamentsFragmentArgs.fromBundle(requireArguments()).tournamentList != null ) {
                 val tournamentSelected =
                     TournamentsFragmentArgs.fromBundle(requireArguments()).tournamentList
                 Log.w(" LISTA FILTRADA !", tournamentSelected.toString())
-            } */
+            }
             // adapter.updateTournaments( tournamentSelected.  )
 
             adapter.updateTournaments(list) // Agregar esta línea para mostrar todas las tarjetas inicialmente
